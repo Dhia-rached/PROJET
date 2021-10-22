526133700afe487116a33b04ded4db9d18f6bf76
azer <azer.bennasr@esprit.tn>
2021-10-22 16:07:03 +0100

    First commit

---
+++ voiture.h
@@ -0,0 +1,12 @@
+#ifndef VOITURE_H
+#define VOITURE_H
+#include<string.h>
+class voiture{
+private:/*
+    string mat;
+    string coul;
+    int j;
+    int m;
+    int ann;*/
+};
+#endif // VOITURE_H
